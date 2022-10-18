da8d6aabc35225f432f3da520b6347fbb94bd6e0
Ifeoluwa9 <agnesolufemi@gmail.com>
2022-10-18 16:52:43 -0700

    get_flags

--- get_flags.c
+++ get_flags.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/**get_flags - This function calculates active flags
+/**
+ * get_flags - This function calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter
  * Return: flags
